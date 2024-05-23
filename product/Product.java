868ca7840fd9426e2e896ebb26fb7c1102c1746b
Sieun Kim <se0983@kookmin.ac.kr>
2024-05-23 09:00:58 +0900

    fix: Product 객체 수정

--- product/Product.java
+++ product/Product.java
@@ -1,11 +1,20 @@
 package com.example.demo.product;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int price;
