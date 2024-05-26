3e85e53ea3b3bb716148d22f4290a0ee0b13fdcc
Sieun Kim <se0983@kookmin.ac.kr>
2024-05-27 00:55:48 +0900

    chore: setting

--- order/OrderController.java
+++ order/OrderController.java
@@ -1,32 +1,32 @@
-package com.example.demo.order;
-
-import com.example.demo.product.Product;
-import com.example.demo.product.ProductService;
-import com.example.demo.utils.Validator;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.ast.Or;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@AllArgsConstructor // 생성자 주입 방법
-public class OrderController {
-    OrderService orderService;
-    ProductService productService;
-
-    @PostMapping("/orders")
-    public void orderProduct(@RequestBody OrderDTO orderDto) {
-        Product orderedProduct = productService.findProduct(orderDto.getProductId()); // 주문상품
-
-        // TODO: Service로 옮길거야 <DTO -> Entity 변환. 생성자 방법.>
-        Order requestOrder = new Order(
-                orderedProduct, orderDto.getCount()
-        );
-
-        orderService.orderProduct(requestOrder);
-    }
-}
+//package com.example.demo.order;
+//
+//import com.example.demo.product.Product;
+//import com.example.demo.product.ProductService;
+//import com.example.demo.utils.Validator;
+//import lombok.AllArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.aspectj.weaver.ast.Or;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@AllArgsConstructor // 생성자 주입 방법
+//public class OrderController {
+//    OrderService orderService;
+//    ProductService productService;
+//
+//    @PostMapping("/orders")
+//    public void orderProduct(@RequestBody OrderDTO orderDto) {
+//        Product orderedProduct = productService.findProduct(orderDto.getProductId()); // 주문상품
+//
+//        // TODO: Service로 옮길거야 <DTO -> Entity 변환. 생성자 방법.>
+//        Order requestOrder = new Order(
+//                orderedProduct, orderDto.getCount()
+//        );
+//
+//        orderService.orderProduct(requestOrder);
+//    }
+//}
--- order/OrderService.java
+++ order/OrderService.java
@@ -1,16 +1,16 @@
-package com.example.demo.order;
-
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@AllArgsConstructor
-public class OrderService {
-    OrderRepository orderRepository;
-
-    public void orderProduct(Order order) {
-//        System.out.println(
-//                "/orders : service - " + order.getProductName());
-        orderRepository.save(order);
-    }
-}
+//package com.example.demo.order;
+//
+//import lombok.AllArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@AllArgsConstructor
+//public class OrderService {
+//    OrderRepository orderRepository;
+//
+//    public void orderProduct(Order order) {
+////        System.out.println(
+////                "/orders : service - " + order.getProductName());
+//        orderRepository.save(order);
+//    }
+//}
